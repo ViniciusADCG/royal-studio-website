61f2735978848dedfcc8bf53764b6153e2429a79
Vinicius Araujo <viniciusaraujocostagoncalves7@gmail.com>
2026-06-26 12:32:13 -0400

    fix: restaura fontes e aplica rastreadores

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,20 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Script from 'next/script'; // Importação adicionada
+import Script from 'next/script';
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
 
 // ... (configuração das fontes)
 
